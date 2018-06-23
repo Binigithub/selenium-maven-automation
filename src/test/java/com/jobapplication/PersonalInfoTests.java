6d1ff443519b470ce0bb56a56aa6e25d8206cf86
CybertekSchool <github@cybertekschool.com>
2018-06-23 14:55:43 -0400

    add vars

--- src/test/java/com/jobapplication/PersonalInfoTests.java
+++ src/test/java/com/jobapplication/PersonalInfoTests.java
@@ -1,6 +1,8 @@
 package com.jobapplication;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,6 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
 import static org.testng.Assert.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,7 +22,7 @@ public class PersonalInfoTests {
 	WebDriver driver;
 	String firstName;
 	String lastName;
-	String gender;
+	int gender;
 	String dateOfBirth;
 	String email;
 	String phoneNumber;
@@ -31,6 +36,8 @@ public class PersonalInfoTests {
 	String github;
 	List<String> certifications;
 	String additionalSkills;
+	Faker data = new Faker();
+	Random random = new Random();
 	
 	@BeforeClass //runs once for all tests
 	public void setUp() {
@@ -45,6 +52,37 @@ public class PersonalInfoTests {
 	public void navigateToHomePage() {
 		System.out.println("Navigating to homepage in @BeforeMethod....");
 		driver.get("https://forms.zohopublic.com/murodil/form/JobApplicationForm/formperma/kOqgtfkv1dMJ4Df6k4_mekBNfNLIconAHvfdIk3CJSQ");	
+		firstName = data.name().firstName();
+		lastName = data.name().lastName();
+		gender = random.nextInt(2)+1;
+		dateOfBirth = data.date().birthday().toString();
+		email = "murodil_ruz@yahoo.com";
+		phoneNumber = data.phoneNumber().cellPhone();
+		city=data.address().cityName();
+		state=data.address().stateAbbr();
+		country=data.address().country();
+		annualSalary=data.number().numberBetween(60000, 150000);
+		technologies = new ArrayList<>();
+		technologies.add("Java-" + data.number().numberBetween(1,4));
+		technologies.add("HTML-" + data.number().numberBetween(1,4));
+		technologies.add("Selenium WebDriver-" + data.number().numberBetween(1,4));
+		technologies.add("TestNG-"+ data.number().numberBetween(1,4));
+		technologies.add("Git-"+ data.number().numberBetween(1,4));
+		technologies.add("Maven-"+ data.number().numberBetween(1,4));
+		technologies.add("JUnit-"+ data.number().numberBetween(1,4));
+		technologies.add("Cucumber-"+ data.number().numberBetween(1,4));
+		technologies.add("API Automation-"+ data.number().numberBetween(1,4));
+		technologies.add("JDBC-"+ data.number().numberBetween(1,4));
+		technologies.add("SQL-"+ data.number().numberBetween(1,4));
+		
+		yearsOfExperience = data.number().numberBetween(0, 11);
+		education = data.number().numberBetween(1, 4)+"";
+		github = "https://github.com/CybertekSchool/selenium-maven-automation.git";
+		certifications = new ArrayList<>();
+		certifications.add("Java OCA");
+		certifications.add("AWS");
+		additionalSkills = data.job().keySkills();
+		
 	}
 	
 	@Test
